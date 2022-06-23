b288e7a018054f997dccf4d4026e6663f7f02e28
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2022-06-22 21:00:50 -0400

    updated template to add middle ware and listen

--- server.js
+++ server.js
@@ -4,22 +4,28 @@ const app = express();
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
-const PORT = process.env.PORT || 8000
+const PORT = 8000
 
 // Require middleware //
 
 // Declare variables //
 let db, 
     dbConnectionString = process.env.DB_STRING,
-    dbName = 'star-wars-quotes',
+    dbName = 'dbName',
     collection 
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
         console.log(`Connected to MongoDb Database`)
         db = client.db(dbName)
-        collection = db.collection('quotes')
+        collection = db.collection('collectionName')
     })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public')) 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
 
 //PORT = 8000
 app.listen(PORT, () => {
